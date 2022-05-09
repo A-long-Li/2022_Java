f838c927f600713432eafe64c18df4913b763558
juanxincai <332209078@qq.com>
2022-05-09 15:02:15 +0800

    多线程输出名字

---
+++ src/pg_test/work_2/printName.java
@@ -0,0 +1,28 @@
+package pg_test.work_2;
+
+import static java.lang.Thread.sleep;
+
+/**
+ * @author liyajun
+ * @date 2022/5/9 14:51
+ */
+public class printName {
+    public static void main(String[] args) {
+        print();
+
+        print();
+    }
+
+    private static void print() {
+        new Thread(() -> {
+            for(int i = 1;i <= 20;i++){
+                System.out.println(Thread.currentThread().getName()+"第"+i+"次打印");
+                try {
+                    sleep(300);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+}
