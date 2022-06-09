94e33b9f76b36b60593f3ce00c846d7b2824b994
juanxincai <332209078@qq.com>
2022-06-09 15:31:59 +0800

    update

--- src/experiment/nine/clientCode.java
+++ src/experiment/nine/clientCode.java
@@ -2,8 +2,8 @@ package experiment.nine;
 
 import java.io.*;
 import java.net.InetAddress;
+import java.net.InetSocketAddress;
 import java.net.Socket;
-import java.net.UnknownHostException;
 import java.util.Scanner;
 
 /**
@@ -11,21 +11,76 @@ import java.util.Scanner;
  * @date 2022/5/11 14:33
  */
 public class clientCode {
-    public static void main(String[] args) throws IOException {
-        Socket s = new Socket(InetAddress.getLocalHost(),9090);
-        OutputStreamWriter sOuter = new OutputStreamWriter(s.getOutputStream());
-        BufferedReader sReader = new BufferedReader(new InputStreamReader(s.getInputStream()));
-        BufferedReader kReader = new BufferedReader(new InputStreamReader(System.in));
-        String line;
-        System.out.println("guess game start");
-        System.out.println("please enter a num");
-        while((line = kReader.readLine()) != null){
-            sOuter.write(line+"\n");
-            sOuter.flush();
-            line = sReader.readLine();
-            System.out.println(line);
-            sOuter.flush();
-        }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Socket mySocket = null;
+        DataInputStream inData = null;
+        DataOutputStream outData = null;
+        ReadNumber readNumber = null;
+        Thread thread;
+        try {
+            mySocket = new Socket();
+            readNumber = new ReadNumber();
+            thread = new Thread(readNumber);
+            System.out.print("输入服务器的IP：");
+            String ip = sc.nextLine();
+            System.out.print("输入端口号：");
+            int port = sc.nextInt();
 
+            if (!mySocket.isConnected()) {
+                InetAddress address = InetAddress.getByName(ip);
+                InetSocketAddress socketAddress = new InetSocketAddress(
+                        address, port);
+                mySocket.connect(socketAddress);
+                InputStream in = mySocket.getInputStream();
+                OutputStream out = mySocket.getOutputStream();
+                inData = new DataInputStream(in);
+                outData = new DataOutputStream(out);
+                readNumber.setDataInputStream(inData);
+                readNumber.setDataOutputStream(outData);
+                thread.start();
+            }
+        } catch (Exception e) {
+            System.out.println("服务器已断开");
+        }
+    }
+}
+
+class ReadNumber implements Runnable {
+    Scanner sc = new Scanner(System.in);
+    DataInputStream in;
+    DataOutputStream out;
+
+    public void setDataInputStream(DataInputStream in) {
+        this.in = in;
+    }
+
+    public void setDataOutputStream(DataOutputStream out) {
+        this.out = out;
+    }
+
+    @Override
+    public void run() {
+        try {
+            out.writeUTF("Y");
+            while (true) {
+                String str = in.readUTF();
+                System.out.println(str);
+                if (!str.startsWith("询问")) {
+                    if (str.startsWith("猜对了")) continue;
+                    System.out.println("好的，我输入猜测：");
+                    int myGuess = sc.nextInt();
+                    String enter = sc.nextLine();//去除多余的回车符
+                    out.writeInt(myGuess);
+                } else {
+                    System.out.println("好的，我输入Y或者N：");
+                    String myAnswer = sc.nextLine();
+                    out.writeUTF(myAnswer);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("服务器已断开");
+        }
     }
 }
--- src/experiment/nine/serverCode.java
+++ src/experiment/nine/serverCode.java
@@ -1,9 +1,6 @@
 package experiment.nine;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Random;
@@ -13,43 +10,78 @@ import java.util.Random;
  * @date 2022/5/11 14:33
  */
 public class serverCode {
-    public static void main(String[] args) throws IOException {
-        Random r = new Random();
-        int ans = r.nextInt(1000)+1;
-        System.out.println("需要猜的数为" + ans);
-        ServerSocket ss = new ServerSocket(9090);
-        Socket s = ss.accept();
-        BufferedReader sReader = new BufferedReader(new InputStreamReader(s.getInputStream()));
-        OutputStreamWriter sOuter = new OutputStreamWriter(s.getOutputStream());
-       // BufferedReader kReader = new BufferedReader(new InputStreamReader(System.in));
-        String line = null;
-        while ((line = sReader.readLine()) != null) {
-            System.out.println("猜的是" + line);
-            int res = Integer.parseInt(line);
-            if(ans == res){
-                sOuter.write("u are right!! \t continue ??? -> enter y to continue \t\n");
-                sOuter.flush();
-                line = sReader.readLine();
-                if(line.startsWith("y") || line.startsWith("Y")) {
-                    sOuter.write("let's go \t please enter a num\t\n");
-                    sOuter.flush();
-                    ans = r.nextInt(1000)+1;
-                    System.out.println("要猜的是"+ans);
-                } else {
-                    sOuter.write("please enter any key to exit\t\n");
-                    sOuter.flush();
-                    s.close();
-                    ss.close();
-                }
-            } else {
-                if(ans > res) {
-                    sOuter.write("too small plz try again \t\n");
-                } else {
-                    sOuter.write("too big plz try again \t\n");
-                }
-                sOuter.flush();
+    public static void main(String[] args) {
+        ServerSocket server = null;
+        Socket you = null;
+        while (true) {
+            try {
+                server = new ServerSocket(5333);
+            } catch (IOException e) {
+                System.out.println("正在监听");
+            }
+            try {
+                you = server.accept();
+                System.out.println("客户的地址：" + you.getInetAddress());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            if (you != null) {
+                new ServerThread(you).start();
             }
         }
-        ss.close();
+    }
+}
+
+class ServerThread extends Thread {
+    Socket socket;
+    DataInputStream in = null;
+    DataOutputStream out = null;
+
+    ServerThread(Socket t) {
+        socket = t;
+        try {
+            out = new DataOutputStream(socket.getOutputStream());
+            in = new DataInputStream(socket.getInputStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void run() {
+        try {
+            while (true) {
+                String str = in.readUTF();
+                boolean boo = str.startsWith("Y") || str.startsWith("y");
+                if (boo) {
+                    out.writeUTF("给你一个1-100的随机数，猜猜它是多少？");
+                    Random ran = new Random();
+                    int realNumber = ran.nextInt(100) + 1;
+                    handleClientGuess(realNumber);
+                    out.writeUTF("询问：想继续玩输入Y，否则输入N：");
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void handleClientGuess(int realNumber) {
+        while (true) {
+            try {
+                int clientGuess = in.readInt();
+                System.out.println(clientGuess);
+                if (clientGuess > realNumber)
+                    out.writeUTF("猜大了");
+                else if (clientGuess < realNumber)
+                    out.writeUTF("猜小了");
+                else {
+                    out.writeUTF("猜对了！");
+                    break;
+                }
+            } catch (IOException e) {
+                System.out.println("客户离开");
+                return;
+            }
+        }
     }
 }
